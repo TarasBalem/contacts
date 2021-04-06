94d4a6d50f473510cc346f7a17c969b257dec668
Taras Balem <balemtaras@gmail.com>
2021-04-06 20:29:10 +0300

    update form with photo

--- src/pages/ContactsPage/components/ContactForm.jsx
+++ src/pages/ContactsPage/components/ContactForm.jsx
@@ -1,11 +1,24 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import PropTypes from "prop-types";
+import ImageLoader from "components/ImageLoader";
 
 const ContactForm = ({saveContact, selectedContact, closeForm}) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
+    phone: "",
+    photo: "",
   });
+
+  const photoRef = useRef();
+
+  const updatePhoto = (e) => {
+    const file = photoRef.current.files && photoRef.current.files[0];
+    if (file) {
+      const img = "/img/" + file.name;
+      setFormData({...formData, photo: img});
+    }
+  };
 
   useEffect(() => {
     if (selectedContact.id) {
@@ -20,48 +33,84 @@ const ContactForm = ({saveContact, selectedContact, closeForm}) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     saveContact(formData);
-    setFormData({name: "", email: ""});
+    setFormData({name: "", email: "", phone: "", photo: ""});
     closeForm();
   };
 
+  const {name, email, phone, photo} = formData;
+
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="container position-relative py-3"
-      style={{maxWidth: "500px"}}
-    >
+    <form onSubmit={handleSubmit} className="container position-relative py-3">
       <button
         type="button"
         className="btn-close position-absolute top-0 end-0"
         onClick={closeForm}
       ></button>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          type="text"
-          className="form-control"
-        />
+      <div className="row">
+        <div className="col-4 d-flex align-items-center justify-content-center">
+          <ImageLoader
+            src={photo}
+            alt={name}
+            fallbackImg="/img/default-avatar.jpg"
+            style={{width: "100%", height: "100%", objectFit: "cover"}}
+          />
+        </div>
+        <div className="col-8">
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              value={name}
+              onChange={handleChange}
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="phone" className="form-label">
+              Phone
+            </label>
+            <input
+              id="phone"
+              name="phone"
+              value={phone}
+              onChange={handleChange}
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="photo" className="form-label">
+              Photo
+            </label>
+            <input
+              id="photo"
+              name="photo"
+              ref={photoRef}
+              onChange={updatePhoto}
+              className="form-control"
+              type="file"
+            />
+          </div>
+          <button className="btn btn-primary">Done</button>
+        </div>
       </div>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">
-          Email
-        </label>
-        <input
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <button className="btn btn-primary">Done</button>
     </form>
   );
 };
